1a06d45a9e13726bd06cf806f5b09095918e8876
Sherwat <72201712+Sherwat@users.noreply.github.com>
2021-02-18 18:30:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    image_quality: 90,
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function takeSnapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+};
+
+console.log('ml5 version: ', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Hjpm23kK8/model.json', modelLoaded);
+
+function modelLoaded(){
+    console.log("!!!Model Loaded!!!");
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data = "The Prediction is" + predection;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
